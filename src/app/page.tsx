0c9821164229ceb4b6873bf7bf7b0dc25cc4ee69
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-08-25 09:02:15 -0300

    feat: add new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { BenefitsSection } from "@/components/BenefitsSection";
 import { CustomizeFurnitureSection } from "@/components/CustomizeFurnitureSection";
+import { FAQ } from "@/components/Faq";
+import { FeedbackSection } from "@/components/FeedbackSection";
+import { Footer } from "@/components/Footer";
+import { FormSection } from "@/components/FormSection";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { InStockItemsSection } from "@/components/inStockItemsSection";
@@ -12,6 +16,10 @@ export default function Home() {
       <BenefitsSection />
       <InStockItemsSection />
       <CustomizeFurnitureSection />
+      <FeedbackSection />
+      <FAQ />
+      <FormSection />
+      <Footer />
     </div>
   );
 }
