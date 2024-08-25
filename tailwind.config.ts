a7d0f89be32a0e84927b77d19dd7a8817878dcab
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-08-25 09:01:43 -0300

    chore: add dependencies and setup tailwind basic and up new images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,17 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        vibrate: {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-2px)' },
+          '50%': { transform: 'translateX(2px)' },
+          '75%': { transform: 'translateX(-2px)' },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        vibrate: 'vibrate 0.3s ease-in-out 1',
       },
     },
   },
