edf5e36e3a36d8b50f49728e5668b8a1a6be163d
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2024-08-25 09:55:34 -0300

    feat: update feedbacks

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -10,39 +10,38 @@ export function FAQ() {
 
   const FaqQuestions: faqObject[] = [
     {
-      answer: 'Temos resinas específicas para ambientes externos que garantem uma proteção contra radiação do sol, solicite acabamento para ambientes externos se pretende deixar o móvel exposto.',
+      answer: 'Sim, oferecemos móveis rústicos projetados especificamente para áreas externas. Usamos acabamentos especiais e resinas que garantem a durabilidade e resistência às condições climáticas.',
       question: 'Existem opções de móveis rústicos para áreas externas, como varandas e jardins?'
     },
     {
-      answer: 'Temos resinas específicas para ambientes externos que garantem uma proteção contra radiação do sol, solicite acabamento para ambientes externos se pretende deixar o móvel exposto.',
+      answer: 'Sim, recomendamos a manutenção regular dos móveis para garantir sua durabilidade. Isso inclui a limpeza com produtos apropriados e a aplicação de acabamentos protetores periodicamente.',
       question: 'Os móveis precisam de alguma manutenção de rotina?'
     },
     {
-      answer: 'Temos resinas específicas para ambientes externos que garantem uma proteção contra radiação do sol, solicite acabamento para ambientes externos se pretende deixar o móvel exposto.',
+      answer: 'Utilizamos uma variedade de madeiras, incluindo o pinho, o carvalho e a nogueira. Cada tipo de madeira oferece características únicas em termos de aparência e durabilidade.',
       question: 'Quais tipos de madeira são usados na fabricação dos móveis rústicos?'
     },
     {
-      answer: 'Temos resinas específicas para ambientes externos que garantem uma proteção contra radiação do sol, solicite acabamento para ambientes externos se pretende deixar o móvel exposto.',
-      question: 'Posso personalizar os móveis  com minhas preferências de cor e acabamento?'
+      answer: 'Sim, você pode personalizar os móveis de acordo com suas preferências. Oferecemos opções de cores, acabamentos e dimensões para atender às suas necessidades específicas.',
+      question: 'Posso personalizar os móveis com minhas preferências de cor e acabamento?'
     },
     {
-      answer: 'Temos resinas específicas para ambientes externos que garantem uma proteção contra radiação do sol, solicite acabamento para ambientes externos se pretende deixar o móvel exposto.',
+      answer: 'Os móveis podem exigir montagem dependendo do modelo. Oferecemos serviço de montagem para sua conveniência, que pode ser solicitado durante o processo de compra.',
       question: 'Os móveis requerem montagem? O serviço de montagem é oferecido?'
     },
     {
-      answer: 'Temos resinas específicas para ambientes externos que garantem uma proteção contra radiação do sol, solicite acabamento para ambientes externos se pretende deixar o móvel exposto.',
+      answer: 'Nossa política de devolução e troca permite que você devolva ou troque o móvel dentro de um prazo específico, desde que ele esteja em condições originais e com a embalagem intacta. Consulte nossa política completa para mais detalhes.',
       question: 'Qual é a política de devolução e troca para móveis?'
     },
     {
-      answer: 'Temos resinas específicas para ambientes externos que garantem uma proteção contra radiação do sol, solicite acabamento para ambientes externos se pretende deixar o móvel exposto.',
+      answer: 'Aceitamos várias formas de pagamento, incluindo cartões de crédito, débito e transferência bancária. Também oferecemos opções de parcelamento para facilitar sua compra.',
       question: 'Quais são as opções de pagamento disponíveis ao comprar móveis rústicos?'
     },
     {
-      answer: 'Temos resinas específicas para ambientes externos que garantem uma proteção contra radiação do sol, solicite acabamento para ambientes externos se pretende deixar o móvel exposto.',
+      answer: 'Nossos móveis são tratados com produtos que ajudam a prevenir a infestação por pragas e insetos. No entanto, é importante realizar inspeções regulares e manter os móveis em condições adequadas.',
       question: 'Os móveis rústicos são resistentes a pragas e insetos?'
     },
-
-  ]
+  ];
   return (
     <section className="bg-neutral-100 py-[120px] px-20 flex justify-center">
       <Container>
